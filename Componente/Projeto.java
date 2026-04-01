1a7e896d319d6af6de79cecf89faefa64a009c6f
thiagodsp-max <tsp@ic.ufal.br>
2026-04-01 16:51:58 -0300

    Enhance comments in Projeto class
    Added comments to clarify attributes and constructor.

---
+++ Componente/Projeto.java
@@ -0,0 +1,16 @@
+//Baseado no conceito de Projeto de Livro
+
+public class Projeto {
+    //Atributos
+    String nome;
+    Pasta[] partes; //Os arquivos ficam armazenados nas cate
+    int count=0;
+
+    //Construtor
+    public Projeto(String titulo){
+        this.nome=titulo;
+        //O array Partes é inicializado com uma Categoria "Padrão" para facilitar o uso
+    }
+    //Métodos a implementar
+    //Método para Adicionar uma nova categoria/novo arquivo
+}
