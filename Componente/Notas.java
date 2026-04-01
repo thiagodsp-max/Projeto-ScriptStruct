8a8717f6db233421c5344b45d6535fde122afd2f
thiagodsp-max <tsp@ic.ufal.br>
2026-04-01 16:47:14 -0300

    Add Notas class for managing annotations

---
+++ Componente/Notas.java
@@ -0,0 +1,25 @@
+//Baseado no Conceito de Anotações
+
+public class Notas {
+    //Atributos
+    String nome;
+    String descricao;
+    boolean box;//Para indicar os status da anotação
+    
+    //Construtor
+    public Notas(String title){
+        this.nome=title;
+        this.box=false;//Inicializa a tarefa como não concluída
+    }
+
+    //Outros métodos
+    public void editarnota(String change){
+        //Para melhor detalhar aquela anotação
+        this.descript=change;
+    }
+  
+    //Método a ser alterado para compreender fenômenos on click()
+    public void concluir(){
+        box=true;//Troca os status da tarefa
+    }
+}
