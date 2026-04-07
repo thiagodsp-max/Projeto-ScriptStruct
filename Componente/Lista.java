ce7a9bbc315827a32e4c9b75a4244286c9aecb33
thiagodsp-max <tsp@ic.ufal.br>
2026-04-07 17:16:27 -0300

    Add comment explaining Lista class functionality
    Added a comment to describe the purpose of the class.

---
+++ Componente/Lista.java
@@ -0,0 +1,7 @@
+//Essa classe vai permitir adicionar diferentes notas, muito útil ao
+//implementar a Tela onde as Anotações serão armazenadas
+
+public class Lista {
+    //Conjunto de Anotações
+    Notas[] tasks;
+}
