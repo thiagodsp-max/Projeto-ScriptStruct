dedae14fdef12a5bb8a0e8c78bfa95316e6aefd7
thiagodsp-max <tsp@ic.ufal.br>
2026-04-21 20:10:57 -0300

    Delete Componente/Lista.java
    Unecessary class

--- Componente/Lista.java
+++
@@ -1,7 +0,0 @@
-//Essa classe vai permitir adicionar diferentes notas, muito útil ao
-//implementar a Tela onde as Anotações serão armazenadas
-
-public class Lista {
-    //Conjunto de Anotações
-    Notas[] tasks;
-}
