bcb33431edd63a940f8346d4e7ddf038e0b9da57
thiagodsp-max <tsp@ic.ufal.br>
2026-04-21 11:41:16 -0300

    Create main application class with JFrame setup
    Implement main class for the application with a basic JFrame setup.

---
+++ Estrutura/App.java
@@ -0,0 +1,20 @@
+//O objetivo é implementar a classe main, criando telas/janelas de acordo com nossa necessidade
+
+import javax.swing.*;
+// Janela principal que implementará as subclasses de Àrea/Janela
+public class App extends JFrame {
+
+    public App() {
+        setSize(600, 500);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null); // centraliza na tela
+
+        // Aqui definimos qual das nossas telas vai ser a inicial
+        setContentPane(new TextEditor());
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new App();
+    }
+}
