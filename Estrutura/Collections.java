4b4f6b733a9ae3f542738a1ff94a667a528c64b9
thiagodsp-max <tsp@ic.ufal.br>
2026-04-22 15:23:20 -0300

    Refactor Collections to use Projeto instead of Livro

--- Estrutura/Collections.java
+++ Estrutura/Collections.java
@@ -1,17 +1,20 @@
 package Struct;
 
-import Beta.Livro;
+import Beta.Projeto;
 
 import javax.swing.*;
 import java.awt.*;
 
 public class Collections extends Base{
-
-    JLabel titulo;
     JPanel libros;
     //Janela feita para armazenar Livros/Lista de Notas
     @Override
     protected void montarCabecalho() {
+        JButton back = new JButton("Lista de Anotações");
+        back.addActionListener(e_->{
+            trocada(new ToDoList());
+        });
+        cabecalho.add(back);
         JLabel titulo = new JLabel("Coleção de Livros");
         cabecalho.add(titulo);
         JButton plus = new JButton("Config");
@@ -35,17 +38,22 @@ public class Collections extends Base{
         livro.addActionListener(e ->{
             String name=JOptionPane.showInputDialog("Nome do Livro");
             if(name!=null && !name.isEmpty()){
-                //Livro view=new Livro(name);
-                //addBook(view);
+                Projeto view=new Projeto(name);
+                addBook(view);
             }
         });
     }
 
-    /*
-    public void addBook(Livro Conto){
+
+    public void addBook(Projeto Conto){
         JPanel bookpanel = new JPanel();
-        bookpanel.setLayout(new BorderLayout());
-        bookpanel.setBorder(BorderFactory.createTitledBorder(Conto.getName()));
+        JButton caderno = new JButton(Conto.getName());
+        caderno.addActionListener(e -> {
+            BookOrder sumario = new BookOrder();
+            trocada(sumario);
+        });
+        bookpanel.add(caderno);
+        libros.add(bookpanel);
+        libros.revalidate();
     }
-     */
 }
