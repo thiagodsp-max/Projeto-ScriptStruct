bb1950758d374293bfef9228020d66867cb441df
thiago <tsp@ic.ufal.br>
2026-04-27 00:55:10 -0300

    Correção de Bugs para o bom Funcionamento do Armazenamento de Arquivos

--- Estrutura/Estrutura/BookOrder.java
+++ Estrutura/Estrutura/BookOrder.java
@@ -13,6 +13,7 @@ public class BookOrder extends Base{
     Projeto biblios;
     JPanel listaPasta;
     //Janela que mostra as Categorias e Arquivos de um Livro
+
     public BookOrder(Projeto book){
         this.biblios=book;
         montarCabecalho("Voltar",book.getName());
@@ -50,7 +51,12 @@ public class BookOrder extends Base{
         pasta.addActionListener(e->{
             String nome=JOptionPane.showInputDialog("Nome da Categoria");
             if(nome != null && !nome.isEmpty()){
-                String caminho=biblios.getName()+"/"+nome;
+                String caminho = biblios.getName() + "/" + nome;
+                String fullPath = Files.BASE_PATH + caminho;
+
+                JOptionPane.showMessageDialog(this,
+                        "Criando pasta em:\n" + fullPath);
+
                 Files.criarPasta(caminho);
                 Pasta pst=new Pasta(nome);
                 addPasta(pst);
@@ -64,14 +70,17 @@ public class BookOrder extends Base{
         JPanel action = new JPanel(new FlowLayout(FlowLayout.RIGHT));
         JLabel nome = new JLabel(pasta.getNome());
         JButton rename = new JButton("F12");
+
+        final String nomeOriginal=pasta.getNome();
+        final String nomePastaAtual=pasta.getNome();
+
         rename.addActionListener(e -> {
-            String nova = JOptionPane.showInputDialog("Novo Nome:", pasta.getNome());
+            String nova = JOptionPane.showInputDialog("Novo Nome:", nomeOriginal);
             if(nova != null && !nova.isEmpty()){
-                String oldPath = biblios.getName() + "/" + pasta.getNome();
+                String oldPath = biblios.getName() + "/" + nomeOriginal;
                 String newPath = biblios.getName() + "/" + nova;
-
+                //JOptionPane.showMessageDialog(this,"Renomeando pasta:\n" +Files.BASE_PATH + oldPath +"\nPARA:\n" +Files.BASE_PATH + newPat);
                 Files.renomear(oldPath, newPath);
-
                 pasta.setNome(nova);
                 nome.setText(nova);
             }
@@ -80,9 +89,10 @@ public class BookOrder extends Base{
 
         JButton remove = new JButton("Deletar");
         remove.addActionListener(e -> {
-            int confirm = JOptionPane.showConfirmDialog(null, "Excluir pasta?");
+            int confirm = JOptionPane.showConfirmDialog(null, "Excluir a pasta ?");
             if(confirm == JOptionPane.YES_OPTION){
-                File pst =new File(Files.BASE_PATH + biblios.getName()+"/"+pasta.getNome());
+                File pst = new File(Files.BASE_PATH + biblios.getName()+"/"+nomePastaAtual);
+                //JOptionPane.showMessageDialog(this,"Deletando pasta:\n" + pst.getAbsolutePath());
                 deletaPasta(pst);
                 listaPasta.remove(bloco);
                 listaPasta.revalidate();
@@ -91,12 +101,14 @@ public class BookOrder extends Base{
         });
         remove.setMargin(new Insets(2,5,2,5));
         JPanel capitulos = new JPanel();
-
         JButton addTexto = new JButton("+ Capítulo");
+
         addTexto.addActionListener(e -> {
             String nomeCap = JOptionPane.showInputDialog("Nome do Texto:");
             if(nomeCap != null && !nomeCap.isEmpty()){
-                String caminho = biblios.getName()+"/"+pasta.getNome()+"/"+nomeCap+".txt";
+                String caminho = biblios.getName()+"/"+nomePastaAtual+"/"+nomeCap+".txt";
+                //String fullPath = Files.BASE_PATH + caminho;
+               // JOptionPane.showMessageDialog(this,"Criando arquivo em:\n" + fullPath);
                 Files.salvarArquivo(caminho,"");
 
                 Arquivo novo = new Arquivo(nomeCap,"");
@@ -136,6 +148,8 @@ public class BookOrder extends Base{
         linha.setMaximumSize(new Dimension(Integer.MAX_VALUE, 40));
         linha.setPreferredSize(new Dimension(0, 40));
         linha.setBorder(BorderFactory.createEmptyBorder(5, 10, 5, 10));
+
+        final String CaminhoOriginal=archive.getCaminho();
         JButton abrir = new JButton(archive.getTitle());
         abrir.addActionListener(e -> {
             TextEditor chapter = new TextEditor(archive,this);
@@ -148,11 +162,11 @@ public class BookOrder extends Base{
         rename.addActionListener(e->{
             String nova = JOptionPane.showInputDialog("Novo titulo", archive.getTitle());
             if(nova != null && !nova.isEmpty()){
-                String old = archive.getCaminho();
-                String pst = old.substring(0, old.lastIndexOf("/"));
+                //String old = archive.getCaminho();
+                String pst = CaminhoOriginal.substring(0, CaminhoOriginal.lastIndexOf("/"));
                 String path = pst + "/" + nova + ".txt";
-
-                Files.renomear(old, path);
+                //JOptionPane.showMessageDialog(this,"Renomeando arquivo:\n" + Files.BASE_PATH + old + "\nPARA:\n" + Files.BASE_PATH + path);
+                Files.renomear(CaminhoOriginal, path);
 
                 archive.setCaminho(path);
                 archive.setTitle(nova);
@@ -164,7 +178,10 @@ public class BookOrder extends Base{
         delete.addActionListener(e -> {
             int confirm = JOptionPane.showConfirmDialog(null, "Excluir arquivo?");
             if(confirm == JOptionPane.YES_OPTION){
-                new java.io.File("repositorio/" + archive.getCaminho()).delete();
+                File f = new File(Files.BASE_PATH + archive.getCaminho());
+                JOptionPane.showMessageDialog(this,
+                        "Deletando arquivo:\n" + f.getAbsolutePath());
+                f.delete();
                 linha.getParent().remove(linha);
                 linha.getParent().revalidate();
                 linha.getParent().repaint();
